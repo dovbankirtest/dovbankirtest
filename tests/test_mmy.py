3ebcee6bb416b8be94cba87e5ea66de9042ec763
dovbankirtest <dovbankir@mail.ru>
2022-04-08 11:25:25 +0300

    rsync 6

--- tests/test_mmy.py
+++ tests/test_mmy.py
@@ -3,10 +3,12 @@ import os
 ver1 = os.getenv('PTESTVERSION', 'noo1')
 ver2 = os.getenv('PTESTVERSION_GA', 'noo2')
 ver3 = os.getenv('MYVAR', 'noo3')
+ver4 = os.getenv('PTESTV', 'noo4')
 
 
 def test1():
     print(' =+=' + ver1)
     print(' =+=' + ver2)
     print(' =+=' + ver3)
+    print(' =+=' + ver4)
     assert ver2 == '6.2.5'
